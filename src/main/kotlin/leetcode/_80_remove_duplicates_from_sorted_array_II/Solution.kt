fbfae0a0cc8c99fc4a150501c00a23ee5a08f08a
Forahdul Islam <fitareq@gmail.com>
2023-06-11 20:03:40 +0600

    created: _80_remove_duplicates_from_sorted_array_II

---
+++ src/main/kotlin/leetcode/_80_remove_duplicates_from_sorted_array_II/Solution.kt
@@ -0,0 +1,21 @@
+package leetcode._80_remove_duplicates_from_sorted_array_II
+
+class Solution {
+    fun removeDuplicates(nums: IntArray): Int {
+        var i = 1
+        for (k in 2 until nums.size){
+            if ((nums[k]!= nums[i] || nums[k]!= nums[i-1])){
+                nums[++i] = nums[k]
+            }
+        }
+        return i+1
+    }
+}
+
+fun main(){
+    val nums1 = intArrayOf(1,1,1,1,1,1,2,3,3,3,4,5,6)
+
+    val solution = Solution()
+
+    println(solution.removeDuplicates(nums1))
+}
