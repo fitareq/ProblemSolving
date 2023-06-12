af8b74c2e00a451ee32b5f7abad2ad6678ee5ea0
Forahdul Islam <fitareq@gmail.com>
2023-06-12 12:56:24 +0600

    created: _169_majority_element

---
+++ src/main/kotlin/leetcode/_169_majority_element/Solution.kt
@@ -0,0 +1,35 @@
+package leetcode._169_majority_element
+
+import kotlin.math.floor
+
+class Solution {
+    fun majorityElement(nums: IntArray): Int {
+        nums.sort()
+        val mid = (nums.size / 2.0).toInt()
+        var count = 1
+        var i = nums[0]
+        for (j in 1 until nums.size) {
+            if (nums[j] == i) {
+                ++count
+            } else {
+                if (count > mid) return i
+                else {
+                    i = nums[j]
+                    count = 1
+                }
+
+            }
+        }
+        return i
+    }
+}
+
+fun main() {
+    val nums1 = intArrayOf(-1,1,1,1,2,1)
+    val nums2 = intArrayOf(2, 5, 6)
+    val m = 3
+    val n = 3
+
+    val solution = Solution()
+    println(solution.majorityElement(nums1))
+}
