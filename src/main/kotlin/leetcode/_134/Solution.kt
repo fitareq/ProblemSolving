e6ac3cf914b5728ce271def5ecf216048bee119b
Forahdul Islam <fitareq@gmail.com>
2023-06-23 22:03:49 +0600

    updated: _134 -solution

--- src/main/kotlin/leetcode/_134/Solution.kt
+++ src/main/kotlin/leetcode/_134/Solution.kt
@@ -1,27 +1,31 @@
 package leetcode._134
 
 class Solution {
-        fun canCompleteCircuit(gas: IntArray, cost: IntArray): Int {
-            var diff = 0
-            var startingStation = -1
-            for (i in gas.indices) {
-                val tmp = gas[i] - cost[i]
-                diff += tmp
-                if (tmp > 0 && startingStation == -1)
-                    startingStation = i
-
+    fun canCompleteCircuit(gas: IntArray, cost: IntArray): Int {
+        var diff = 0
+        var tmpDiff = 0
+        var startingStation = 0
+        for (i in gas.indices) {
+            val tmp = gas[i] - cost[i]
+            diff += tmp
+            tmpDiff += tmp
+            if (tmpDiff < 0) {
+                tmpDiff = 0
+                startingStation = i + 1
             }
 
-            return if (diff >= 0) startingStation else -1
         }
+
+        return if (diff >= 0) startingStation else -1
+    }
 }
 
 fun main() {
-    val gas = intArrayOf(1,2,3,4,5)
-    val cost = intArrayOf(3,4,5,1,2)
+//    val gas = intArrayOf(1,2,3,4,5)
+//    val cost = intArrayOf(3,4,5,1,2)
 
-//    val gas = intArrayOf(2,3,4)
-//    val cost = intArrayOf(3,4,3)
+    val gas = intArrayOf(2, 3, 4)
+    val cost = intArrayOf(3, 4, 3)
 
 //    val gas = intArrayOf(5,1,2,3,4)
 //    val cost = intArrayOf(4,4,1,5,1)
